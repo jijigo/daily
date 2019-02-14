24ff60dc0cd75d6a3e958bfdc1a594a92379e19c
debbyji <jasmine820921@gmail.com>
2019-02-15 01:42:06 +0800

    Updates

---
+++ precache-manifest.9aa50c0073378425beaefab5c78e0a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "faf038214f9d2ec886bf",
+    "url": "/react-todolist/static/css/main.55fdb2a1.chunk.css"
+  },
+  {
+    "revision": "faf038214f9d2ec886bf",
+    "url": "/react-todolist/static/js/main.faf03821.chunk.js"
+  },
+  {
+    "revision": "618be408886a6d21caf8",
+    "url": "/react-todolist/static/js/runtime~main.618be408.js"
+  },
+  {
+    "revision": "90705ce397e72243e937",
+    "url": "/react-todolist/static/css/2.f604eee0.chunk.css"
+  },
+  {
+    "revision": "90705ce397e72243e937",
+    "url": "/react-todolist/static/js/2.90705ce3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90daa2295b3d38e36ff7b3a1442865ad",
+    "url": "/react-todolist/index.html"
+  }
+];
